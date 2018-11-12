5c54b2f5b17feb1461533fff31bf4ccd3b558be4
Ethan <ethantran7@yahoo.com>
2018-11-12 01:48:37 -0600

    Geometry commit
    Added the Plane, Polygon, and Ray classes. They don't do much, yet.

---
+++ RunJumpSwingMan/src/Framework/Geometry/Plane.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+namespace RunJumpSwingMan.src.Framework.Geometry {
+	class Plane {
+
+		public Vector3 origin;
+		public Vector3 normal;
+
+		public Plane(Vector3 ori, Vector3 norm) {
+			origin = ori;
+			normal = norm;
+		}
+
+		
+	}
+}
---
+++ RunJumpSwingMan/src/Framework/Geometry/Ray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+namespace RunJumpSwingMan.src.Framework.Geometry {
+	class Ray {
+
+		public Vector3 origin;
+		public Vector3 direction;
+
+		public Ray(Vector3 ori, Vector3 dir) {
+			origin = ori;
+			direction = dir;
+		}
+
+		Vector3? intersection(Plane plane) {
+			if (Vector3.Dot(direction, plane.normal) == 0) {
+				return null;
+			}
+
+			float length = (Vector3.Dot(plane.normal, plane.origin) - Vector3.Dot(plane.normal, origin)) / (Vector3.Dot(plane.normal, direction));
+			return origin + direction * length;
+		}
+
+	}
+}
